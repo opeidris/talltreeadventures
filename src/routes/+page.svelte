5eb2096eebdd9503f570f534065dc1c6594aa479
opeidris <opeyemi@opeidris.com>
2024-05-17 15:55:22 +0800

    updated small spelling changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
     'start': "At the start of your quest, help arrives from metal beasts. Seek where they rest when tasks are done, vehicles groomed one by one. This is the first clue so I'll let you off easy, head to 57 John Street, Northbridge",
     'let': "Blades fly true when aimed with skill, At wooden targets, thrill after thrill. This modern sport's primal cry, Points where axes rent the sky. One more hint, it's really close by.",
     'the': "Where ancient relics find their home, Preserved for all to explore and roam. Exhibits rich with history's trace, Guide you to this hallowed place.",
-    '5683': "Welcome to Tall Tree Adventures Natasha and Jacinta! You're booked in a for a whole days adventure around Perth city, so first let me give you your instructions. Throughout the day you will receive cards such as the one you are holding. Each will have a keyword on it. Type in the keyword and it will provide the next clue. If you retype the keyword, the clue will repeat itself. Type 'clear' to clear the chat's on the screen and to make space. To get started, clear the chat if your wish then, type your first keyword 'Start'",
+    '5683': "Welcome to Tall Tree Adventures Natasha and Jacinta! You're booked in a for a whole days adventure around Perth city, so first let me give you your instructions. Throughout the day you will receive cards such as the one you are holding. Each will have a keyword on it. Type in the keyword and it will provide the next clue. If you retype the keyword, the clue will repeat itself. Type 'clear' to clear the messages on the screen and to make space. To get started, clear the chat if your wish, then type your first keyword 'Start'. Remember to bring the clue cards around with you for the day.",
     'show': "Worlds of stories, fact and fiction, Bound between covers, a literary addiction. This planet's orbital pull is strong,   Attracting readers where tales belong.",
     'world.': "Russell Square's greenery hides, A lock that your key's use abides. From humble origins did it start, That metal prize must play its part. When boughs with purple blossoms bloom, A vivid scene for all to consume.",
     'me': "Secrets unlocked by your key's turn, Yield one more, its use you'll soon learn. To a sanctuary of love you must go, Where crimson robes await their ruby glow. A cherished place, your next warm phase, That familiar domestic base.",
@@ -40,7 +40,7 @@
 </script>
 
 <div class=" min-h-screen flex flex-col items-center bg-yellow-200">
-  <h1 class="text-5xl md:text-6xl barlow-semi-condensed-extrabold-italic text-blue-950 my-4">Tall Tree Adventures</h1>
+  <h1 class="text-5xl md:text-6xl barlow-semi-condensed-extrabold-italic text-blue-950 my-4 text-center">Tall Tree Adventures</h1>
   <div class="f mt-8 flex flex-col w-5/6 md:w-1/4">
     {#each messages as message}
       <div class={message.sender== 'bot' ? 'text-gray-50 bg-green-500 rounded-lg p-3 w-3/5 mb-4 self-end' : ' text-gray-50 bg-blue-500 rounded-lg p-3 w-3/5 mb-4 self-start mr-auto'}>
